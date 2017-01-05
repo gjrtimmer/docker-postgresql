3f61d64147fac2f9e51a5b8b43568f4562485978
G.J.R. Timmer <gjr.timmer@gmail.com>
2017-01-05 20:01:05 +0100

    added functions

--- rootfs/etc/profile.d/functions.sh
+++ rootfs/etc/profile.d/functions.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bash
+# Functions taken from sameersbn/docker-postgresql
 set -e
 
 source /etc/postgresql/env-defaults
@@ -6,6 +7,53 @@ source /etc/postgresql/env-defaults
 ## Execute command as PG_USER
 exec_as_postgres() {
   sudo -HEu postgres "$@"
+}
+
+set_postgresql_param() {
+	local key=${1}
+	local value=${2}
+	local verbosity=${3:-verbose}
+
+	if [[ -n ${value} ]]; then
+		local current=$(s6-setuidgid ${PG_USER} sed -n -e "s/^\(${key} = '\)\([^ ']*\)\(.*\)$/\2/p" ${PG_CONF})
+		
+		if [[ "${current}" != "${value}" ]]; then
+			
+			if [[ ${verbosity} == verbose ]]; then
+				echo "  Setting postgresql.conf parameter: ${key} = '${value}'"
+			fi
+	
+			value="$(echo "${value}" | sed 's|[&]|\\&|g')"
+			s6-setuidgid ${PG_USER} sed -i "s|^[#]*[ ]*${key} = .*|${key} = '${value}'|" ${PG_CONF}
+		fi
+	fi
+}
+
+set_recovery_param() {
+	local key=${1}
+	local value=${2}
+	local hide=${3}
+  
+	if [[ -n ${value} ]]; then
+		local current=$(s6-setuidgid ${PG_USER} sed -n -e "s/^\(.*\)\(${key}=\)\([^ ']*\)\(.*\)$/\3/p" ${PG_RECOVERY_CONF})
+
+		if [[ "${current}" != "${value}" ]]; then
+			case ${hide} in
+				true)  	echo "  Setting primary_conninfo parameter: ${key}" ;;
+				*) 		echo "  Setting primary_conninfo parameter: ${key} = '${value}'" ;;
+			esac
+
+			s6-setuidgid ${PG_USER} sed -i "s|${key}=[^ ']*|${key}=${value}|" ${PG_RECOVERY_CONF}
+		fi
+	fi
+}
+
+set_hba_param() {
+	local value=${1}
+
+	if ! grep -q "$(sed "s| | \\\+|g" <<< ${value})" ${PG_HBA_CONF}; then
+		echo "${value}" >> ${PG_HBA_CONF}
+	fi
 }
 
 # EOF
