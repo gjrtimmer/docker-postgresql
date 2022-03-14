b6b8654e6862b16da762e4b4720357f04d7c4d5a
G.J.R. Timmer <gjr.timmer@gmail.com>
2022-03-14 20:03:05 +0800

    fix: update snapshot

--- examples/master/snapshot.sh
+++ examples/master/snapshot.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Create Backup of master
+# Create snapshot of master
 docker run \
   --rm \
-  --name psql-backup \
+  --name psql-snapshot \
   --network psql-playground \
   --link psql-master:master \
   --env REPLICATION_USER=replicator \
   --env REPLICATION_PASS=replicator \
   --env REPLICATION_HOST=master \
   --env REPLICATION_PORT=5432 \
-  --env REPLICATION_MODE=backup \
-  --volume "${PWD}/backup/postgresql.$(date +%Y%m%d-%H%M):/config" \
+  --env REPLICATION_MODE=snapshot \
+  --volume "${PWD}/snapshot:/config" \
   gjrtimmer/postgresql:latest
